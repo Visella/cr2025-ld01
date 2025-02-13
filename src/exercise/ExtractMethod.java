8ba6dbb657a0b75f79a82dc3ea940530b932b2d8
Visella <136889489+Visella@users.noreply.github.com>
2025-02-13 16:57:13 +0700

    pisah print banner, details, hitung outstanding

--- src/exercise/ExtractMethod.java
+++ src/exercise/ExtractMethod.java
@@ -18,26 +18,53 @@ public class ExtractMethod {
 		orders = new Order();
 	}
 	// TODO: reduce this method with extract method
+//	void printOwing() {
+//	  Enumeration elements = orders.elements();
+//	  double outstanding = 0.0;
+//
+//	  // print banner
+//	  System.out.println ("*****************************");
+//	  System.out.println ("****** Customer totals ******");
+//	  System.out.println ("*****************************");
+//
+//	  // print owings
+//	  while (elements.hasMoreElements()) {
+//	    Order each = (Order) elements.nextElement();
+//	    outstanding += each.getAmount();
+//	  }
+//
+//	  // print details
+//	  System.out.println("name: " + name);
+//	  System.out.println("amount: " + outstanding);
+//	}
+	
 	void printOwing() {
-	  Enumeration elements = orders.elements();
-	  double outstanding = 0.0;
-
-	  // print banner
-	  System.out.println ("*****************************");
-	  System.out.println ("****** Customer totals ******");
-	  System.out.println ("*****************************");
-
-	  // print owings
-	  while (elements.hasMoreElements()) {
-	    Order each = (Order) elements.nextElement();
-	    outstanding += each.getAmount();
-	  }
-
-	  // print details
-	  System.out.println("name: " + name);
-	  System.out.println("amount: " + outstanding);
+        printBanner();
+        double outstanding = calculateOutstanding();
+        printDetails(outstanding);
+    }
+	
+	void printBanner() {
+		  System.out.println ("*****************************");
+		  System.out.println ("****** Customer totals ******");
+		  System.out.println ("*****************************");
 	}
 	
+	private double calculateOutstanding() {
+        Enumeration elements = orders.elements();
+        double outstanding = 0.0;
+        while (elements.hasMoreElements()) {
+            Order each = (Order) elements.nextElement();
+            outstanding += each.getAmount();
+        }
+        return outstanding;
+    }
+	
+	 private void printDetails(double outstanding) {
+	        System.out.println("name: " + name);
+	        System.out.println("amount: " + outstanding);
+	    }
+	 
 	class Order implements Enumeration {
 		private double [] amounts;
 		private int currentIndex;
