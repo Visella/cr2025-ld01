699992aecb98db6bcec208ed93d29c15c9396f4e
Visella <136889489+Visella@users.noreply.github.com>
2025-02-13 16:55:52 +0700

    Pisah getTicketPrice, isSummer, calculateWinterPrice, calculateSummerPrice

--- src/exercise/DecomposeConditional.java
+++ src/exercise/DecomposeConditional.java
@@ -18,16 +18,40 @@ public class DecomposeConditional {
 		  
 		  // TODO: change the conditional logic in this method using
 		  //       decompose conditional
-		  public double getTicketPrice(Date date, int quantity) {
-		    double charge;
-		    if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
-		      charge = quantity * winterRate + winterServiceCharge;
-		    }
-		    else {
-		      charge = quantity * summerRate;
-		    }
-		    return charge;
-		  }
+		  
+		  // Decompose conditional: memisahkan condiition menjadi method-method sendiri 
+		  // dan memisahkan action di dalam condition ke dalam method sendiri
+		  
+		  private boolean isSummer(Date date) {
+			    return !date.before(SUMMER_START) && !date.after(SUMMER_END);
+			}
+
+		  private double calculateWinterPrice(int quantity) {
+			    return quantity * winterRate + winterServiceCharge;
+			}
+
+			private double calculateSummerPrice(int quantity) {
+			    return quantity * summerRate;
+			}
+
+			public double getTicketPrice(Date date, int quantity) {
+	            if (isSummer(date)) {
+	                return calculateSummerPrice(quantity);
+	            } else {
+	                return calculateWinterPrice(quantity);
+	            }
+	        }
+		  
+//		  public double getTicketPrice(Date date, int quantity) {
+//		    double charge;
+//		    if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
+//		      charge = quantity * winterRate + winterServiceCharge;
+//		    }
+//		    else {
+//		      charge = quantity * summerRate;
+//		    }
+//		    return charge;
+//		  }
 		}
 	public void test() {
 		Stadium s = new Stadium(100, 90, 25);
