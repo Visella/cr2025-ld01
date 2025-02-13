479e347c913f5f494de03231ab0ce17c308d3451
Visella <136889489+Visella@users.noreply.github.com>
2025-02-13 16:56:27 +0700

    pisah class telephone number

--- src/exercise/ExtractClass.java
+++ src/exercise/ExtractClass.java
@@ -1,28 +1,47 @@
 package exercise;
 public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
+	
+	class TelephoneNumber {
+        private String officeAreaCode;
+        private String officeNumber;
+
+        public String getOfficeAreaCode() {
+            return officeAreaCode;
+        }
+
+        public void setOfficeAreaCode(String officeAreaCode) {
+            this.officeAreaCode = officeAreaCode;
+        }
+
+        public String getOfficeNumber() {
+            return officeNumber;
+        }
+
+        public void setOfficeNumber(String officeNumber) {
+            this.officeNumber = officeNumber;
+        }
+
+        public String getTelephoneNumber() {
+            return "(" + officeAreaCode + ") " + officeNumber;
+        }
+    }
+	
 	class Person {
 	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
-
+	  private TelephoneNumber telephoneNumber = new TelephoneNumber();
+	  
 	  public String getName() {
 		return name;
 	  }
+	  
 	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
-	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
+          return telephoneNumber.getTelephoneNumber();
+      }
+
+      public TelephoneNumber getTelephoneNumberObject() {
+          return telephoneNumber;
+      }
+	  
 	}
 }
